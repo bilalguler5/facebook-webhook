998ca2e2e0a01183d6a3a036ab4c022e5fd181d5
bilalguler5 <bilalguler5@gmail.com>
2025-06-24 15:28:57 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,46 @@
+const express = require("express");
+const axios = require("axios"); // Make'e veri göndermek için
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
+
+const VERIFY_TOKEN = "Allah1dir.,";
+
+// Webhook doğrulama
+app.get("/webhook", (req, res) => {
+  const mode = req.query["hub.mode"];
+  const token = req.query["hub.verify_token"];
+  const challenge = req.query["hub.challenge"];
+
+  if (mode === "subscribe" && token === VERIFY_TOKEN) {
+    console.log("✅ Webhook doğrulandı.");
+    res.status(200).send(challenge);
+  } else {
+    console.warn("⛔ Webhook doğrulama başarısız.");
+    res.sendStatus(403);
+  }
+});
+
+// Facebook'tan gelen veriyi Make'e gönder
+app.post("/webhook", async (req, res) => {
+  console.log("📨 Facebook'tan veri geldi:", JSON.stringify(req.body, null, 2));
+
+  try {
+    await axios.post(
+      "https://hook.us2.make.com/jpkfwm4kjvpdjly72jciots7wtevnbx8",
+      req.body,
+    );
+    console.log("✅ Veri Make'e gönderildi.");
+  } catch (error) {
+    console.error("🚨 HATA:", error.message);
+  }
+
+  res.sendStatus(200);
+});
+
+// Sunucuyu başlat
+app.listen(PORT, () => {
+  console.log(`🚀 Server aktif: http://localhost:${PORT}`);
+});
